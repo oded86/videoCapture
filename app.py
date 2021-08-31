b8657c9881f9b121ad98514f13af13e924bbfffd
oded <oded86@gmail.com>
2021-08-31 16:48:07 +0300

    added try: except:

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ success,image = vidcap.read()
 
 success = True
 while success:
-  success,image = vidcap.read()
-  cv2.imwrite(file_name, image)     # save frame as JPEG file
-  success= False
-  if cv2.waitKey(10) == 27:                     # exit if Escape is hit
-      break
+    try:
+        success,image = vidcap.read()
+        cv2.imwrite(file_name, image)     # save frame as JPEG file
+        success= False
+
+    except:
+        print ("There was an Error")
